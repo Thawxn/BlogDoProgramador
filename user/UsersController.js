5daad1563d62adbac1d9c2ca247ee280c9a49a16
Thawan Marques <thawan28032001@gmail.com>
2022-09-19 23:35:00 -0300

    five commit

--- user/UsersController.js
+++ user/UsersController.js
@@ -17,12 +17,13 @@ router.post('/admin/create', function(req,res){
     var email = req.body.email;
     var password = req.body.password;
 
-    var salt = bcrypt.genSaltSync(10);
-    var hash = bcrypt.hashSync(password, salt);
 
     User.findOne({where:{email:email}}).then( user => {
 
         if(user == undefined){
+            var salt = bcrypt.genSaltSync(10);
+            var hash = bcrypt.hashSync(password, salt);
+
             User.create({
                 email: email,
                 password: hash
@@ -32,9 +33,40 @@ router.post('/admin/create', function(req,res){
                 res.redirect('/')
             });
         } else {
-            res.redirect('../admin/users/create');
+            res.redirect('/admin/users/create');
         };
     });
 });
+
+router.get('/login', function(req,res){
+    res.render('admin/users/login')
+});
+
+router.post('/authenticate', function(req,res){
+    var email = req.body.email;
+    var password = req.body.password;
+
+    User.findOne({
+        where: {email: email}
+    }).then(user => {
+        if(user != undefined){
+            var correct = bcrypt.compareSync(password, user.password);
+            if(correct){
+                req.session.user = {
+                    id: user.id,
+                    email: user.email
+                }
+                res.json(req.session.user)
+            }else{
+                res.redirect('/login')
+            }
+
+        }else{
+            res.redirect('/login')
+        }
+
+
+    })
+})
 
 module.exports = router;

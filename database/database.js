eb6acedcb1ea5083fcf629130ea2e20faabb15a2
Thawan Marques <111232361+Thawxn@users.noreply.github.com>
2022-12-07 17:57:43 -0300

    Update database.js

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const connection = new Sequelize('guiapress', 'root', '********.',{
+const connection = new Sequelize('guiapress', 'root', '2001',{
     host: 'localhost',
     dialect: 'mysql',
     timezone: '-3:00'
